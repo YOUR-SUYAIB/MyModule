59952909b9e483e74f29bcc583e96541ab84c0d0
YOUR-SUYAIB <nime02222@gmail.com>
2024-08-09 01:45:30 +0600

    Create your-suyaib.py

---
+++ your-suyaib.py
@@ -0,0 +1,14 @@
+# myinfo.py
+
+name = "MD SUYAIB ISLAM"
+telegram_id = "@Unknown_X05"
+channel = "Dark Team Termux Exploration"
+github = "Your-Suyaib"
+
+def get_info():
+    return {
+        "Name": name,
+        "Telegram ID": telegram_id,
+        "Channel": channel,
+        "Github": github
+    }
